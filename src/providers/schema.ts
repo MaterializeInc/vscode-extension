b47e7e2b2454607687ede76a9a964db9325d869c
Bobby Iliev <bobby@bobbyiliev.com>
2023-11-08 13:41:05 +0200

    136 Sort explorer objects by name (#141)

--- src/providers/schema.ts
+++ src/providers/schema.ts
@@ -163,7 +163,7 @@ export default class DatabaseTreeProvider implements vscode.TreeDataProvider<Nod
 
     private async getSources(schema: String): Promise<Array<Node>> {
         const sources: Promise<Array<Node>> = new Promise((res, rej) => {
-            this.query("SELECT id, name, owner_id FROM mz_sources WHERE schema_id = $1", [schema]).then((results) => {
+            this.query("SELECT id, name, owner_id FROM mz_sources WHERE schema_id = $1 ORDER BY name", [schema]).then((results) => {
                 let sources = results.map(({ id, name, owner_id: ownerId }) => {
                     return new View(name, vscode.TreeItemCollapsibleState.Collapsed, { id, name, ownerId });
                 });
@@ -176,7 +176,7 @@ export default class DatabaseTreeProvider implements vscode.TreeDataProvider<Nod
 
     private async getSinks(schema: String): Promise<Array<Node>> {
         const sinks: Promise<Array<Node>> = new Promise((res, rej) => {
-            this.query("SELECT id, name, owner_id FROM mz_sinks WHERE schema_id = $1", [schema]).then((results) => {
+            this.query("SELECT id, name, owner_id FROM mz_sinks WHERE schema_id = $1 ORDER BY name", [schema]).then((results) => {
                 let sinks = results.map(({ id, name, owner_id: ownerId }) => {
                     return new View(name, vscode.TreeItemCollapsibleState.Collapsed, { id, name, ownerId });
                 });
@@ -189,7 +189,7 @@ export default class DatabaseTreeProvider implements vscode.TreeDataProvider<Nod
 
     private async getViews(schema: String): Promise<Array<Node>> {
         const views: Promise<Array<Node>> = new Promise((res, rej) => {
-            this.query("SELECT id, name, owner_id FROM mz_views WHERE schema_id = $1", [schema]).then((results) => {
+            this.query("SELECT id, name, owner_id FROM mz_views WHERE schema_id = $1 ORDER BY name", [schema]).then((results) => {
                 let views = results.map(({ id, name, owner_id: ownerId }) => {
                     return new View(name, vscode.TreeItemCollapsibleState.Collapsed, { id, name, ownerId });
                 });
@@ -202,7 +202,7 @@ export default class DatabaseTreeProvider implements vscode.TreeDataProvider<Nod
 
     private async getMaterializedViews(schema: String): Promise<Array<Node>> {
         const materializedViews: Promise<Array<Node>> = new Promise((res, rej) => {
-            this.query("SELECT id, name, owner_id, cluster_id FROM mz_materialized_views WHERE schema_id = $1", [schema]).then((results) => {
+            this.query("SELECT id, name, owner_id, cluster_id FROM mz_materialized_views WHERE schema_id = $1 ORDER BY name", [schema]).then((results) => {
                 let materializedViews = results.map(({ id, name, owner_id: ownerId }) => {
                     return new MaterializedView(name, vscode.TreeItemCollapsibleState.Collapsed, { id, name, ownerId });
                 });
@@ -215,7 +215,7 @@ export default class DatabaseTreeProvider implements vscode.TreeDataProvider<Nod
 
     private async getTables(schema: String): Promise<Array<Node>> {
         const tables: Promise<Array<Node>> = new Promise((res, rej) => {
-            this.query("SELECT id, name, owner_id FROM mz_tables WHERE schema_id = $1", [schema]).then((results) => {
+            this.query("SELECT id, name, owner_id FROM mz_tables WHERE schema_id = $1 ORDER BY name", [schema]).then((results) => {
                 let tables = results.map(({ id, name, owner_id: ownerId }) => {
                     return new Table(name, vscode.TreeItemCollapsibleState.Collapsed, { id, name, ownerId });
                 });
@@ -228,7 +228,7 @@ export default class DatabaseTreeProvider implements vscode.TreeDataProvider<Nod
 
     private async getSchemas(database: String): Promise<Array<Schema>> {
         return new Promise((res, rej) => {
-            this.query("SELECT id, name, database_id, owner_id FROM mz_schemas WHERE database_id = $1", [database]).then((results) => {
+            this.query("SELECT id, name, database_id, owner_id FROM mz_schemas WHERE database_id = $1 ORDER BY name", [database]).then((results) => {
                 const schemas = results.map(({ id, name, owner_id: ownerId }) => {
                     return new Schema(name, vscode.TreeItemCollapsibleState.Collapsed, { id, name, ownerId });
                 });
@@ -239,7 +239,7 @@ export default class DatabaseTreeProvider implements vscode.TreeDataProvider<Nod
 
     private async getColumns(id: String): Promise<Array<Column>> {
         return new Promise((res, rej) => {
-            this.query("SELECT name, type FROM mz_columns WHERE id = $1", [id]).then((results) => {
+            this.query("SELECT name, type FROM mz_columns WHERE id = $1 ORDER BY name", [id]).then((results) => {
                 const columns = results.map(({ name, type }) => {
                     return new Column(name, vscode.TreeItemCollapsibleState.None, { id: name, type });
                 });
